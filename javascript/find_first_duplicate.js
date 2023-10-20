dbc498113e4f87fefecd97e8e885b6e585372650
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-10-20 10:32:03 -0400

    find first duplicate done

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,6 +1,21 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  const seen = {};
+  for (let element of arr) {
+    if (seen[element]) return element;
+    seen[element] = 1;
+  }
+  return -1;
 }
+
+//Set solution below:
+// function findFirstDuplicate(arr) {
+//   const seen = new Set();
+//   for (let element of arr) {
+//     if (seen.has(element)) return element;
+//     seen.add(element);
+//   }
+//   return -1;
+// }
 
 if (require.main === module) {
   // add your own tests in here
@@ -11,9 +26,23 @@ if (require.main === module) {
 
   console.log("Expecting: -1");
   console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
+
+  console.log("expecting 4");
+  console.log(findFirstDuplicate([1, 4, 5, 8, 9, 4]));
+
+  console.log("expecting 2");
+  console.log(findFirstDuplicate([2, 3, 5, 2, 3]));
 }
 
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/* 
+Problem: given an array, find the first element that appears more than once. return -1 if none are found
+create object to store array elements
+loop through the array
+if object contains element, return that element.
+otherwise, add it to the object as a key with the value 1;
+return -1 at the end.
+*/
